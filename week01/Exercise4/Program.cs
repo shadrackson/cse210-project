ea36f64bdcc997e72332c6344a7245d963e2f05e
shadrackson <sonyango@byupathway.edu>
2026-07-11 23:30:52 +0300

    did modification

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int sum = 0;
+
+        foreach (int item in numbers)
+        {
+            sum += item;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbers[0];
+
+        foreach (int item in numbers)
+        {
+            if (item > largest)
+            {
+                largest = item;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
+
