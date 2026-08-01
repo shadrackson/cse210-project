b91677cb41f9108dcd766312a8fdb3c9497a335b
Shadrack Onyango <shadrackboss442@gmail.com>
2026-08-01 19:17:30 +0300

    Enhance Comment class with constructor and getters
    Added constructor and getter methods for Comment class.

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,28 @@
+// My Visual Studio Code keeps hanging and breaking
+// I have decided to utilize github instead
+
+
+public class Comment
+{
+  private string _name;
+  private string _text;
+}
+
+public Comment(string name, string text)
+{
+  _name = name;
+  _text = text;
+}
+
+public string GetName()
+{
+  return _name;
+}
+
+public string GetText()
+{
+  return _text;
+}  
+  
+
+  
