e20345645b4b7eb2cb4c96b198417d9d0ae22656
Shadrack Onyango <shadrackboss442@gmail.com>
2026-08-01 21:56:50 +0300

    Add Address class with constructor and methods
    Implemented constructors and methods in the address class

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,33 @@
+// My VS Code is not working as intended
+// I am using the github space for my code
+
+// Class
+public class Address
+{
+  private string _street;
+  private string _city;
+  private string _stateProvince;
+  private string _country;
+}
+
+// Constructor
+public Address(string street, string city, string stateProvince, string country)
+{
+  _street = street;
+  _city = city;
+  _stateProvince = stateProvince;
+  _country = country;
+}
+
+// Methods
+public bool IsInUSA()
+{
+  return _country.ToUpper() == "USA";
+}
+
+public string GetFullAddress()
+{
+  return $"{_street}\n{_city}, {_stateProvince}\n{_country};
+}   
+
+
