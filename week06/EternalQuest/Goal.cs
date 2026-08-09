bf5f2939dc205b74afb64a52bf4e8fd67215f412
Shadrack Onyango <shadrackboss442@gmail.com>
2026-08-10 00:27:38 +0300

    Refine comments in Goal.cs for better understanding
    Updated comments for clarity and consistency in Goal.cs.

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,12 +1,15 @@
-// This is the base class for all goals.
+ // This is the base class for all types of goals.
+// It contains information and behavior that all goals share.
 public abstract class Goal
 {
-    // Private variables helps us practice encapsulation.
+    // Private variables demonstrate encapsulation.
+    // These variables can only be accessed directly inside this class.
     private string _name;
     private string _description;
     private int _points;
 
-    // Constructor
+    // Constructor used to create a goal.
+    // The information is provided when a goal is created.
     public Goal(string name, string description, int points)
     {
         _name = name;
@@ -14,7 +17,8 @@ public abstract class Goal
         _points = points;
     }
 
-    // Properties allow other classes to read the information.
+    // These properties allow other classes to read
+    // the information stored in the private variables.
     public string Name
     {
         get { return _name; }
@@ -30,10 +34,19 @@ public abstract class Goal
         get { return _points; }
     }
 
-    // Each type of goal will implement these differently.
-    public abstract int RecordEvent();
-
+    // Determines whether the goal has been completed.
+    // Each type of goal will implement this differently.
     public abstract bool IsComplete();
 
-    public abstract string GetDetailsString();
-}
+    // Records an event and returns the number of points earned.
+    // Each type of goal will override this method.
+    public abstract int RecordEvent();
+
+    // Creates the text that will be displayed when
+    // the user chooses to see their goals.
+    public abstract string GetDisplayString();
+
+    // Converts the goal into text that can be saved to a file.
+    public abstract string GetSaveString();
+} 
+
